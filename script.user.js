3ae178a0fdcb593e70e85bc7b149636b46c2a892
刘政东 <735083049@qq.com>
2023-05-06 11:39:23 +0800

    更新2.5版本
    - UI 调整，删除原本的依赖 🆗
    - 跟踪说明 🆗

--- script.user.js
+++ script.user.js
@@ -5,23 +5,27 @@
 // @match       https://m.douban.com/movie/*
 // @grant       GM_addStyle
 // @grant       GM_xmlhttpRequest
+// @antifeature tracking
 // @connect     *
 // @run-at      document-end
-// @require     https://cdn.jsdelivr.net/npm/xy-ui@1.10.7/+esm
 // @require     https://cdn.staticfile.org/artplayer/4.6.2/artplayer.min.js
 // @require     https://unpkg.com/hls.js@1.2.9/dist/hls.min.js
-// @version     2.4
+// @version     2.5
 // @author      liuser, collaborated with ray
 // @description 想看就看
 // @license MIT
 // ==/UserScript==
 
+
+
+
 (function () {
     const _debug = 0;
     let art = {}; //播放器
     let seriesNum = 0;
+    let sourceSelected = false;
     const { query: $, queryAll: $$, isMobile } = Artplayer.utils;
-    const tip = (message) => XyMessage.info(message);
+    const tip = (message) => alert(message);
 
     //获取豆瓣影片名称
     const videoName = isMobile ? $(".sub-title").innerText : document.title.slice(0, -5);
@@ -47,16 +51,16 @@
 
     //搜索源
     const searchSource = [
-        // {"name":"闪电资源","searchUrl":"https://sdzyapi.com/api.php/provide/vod/"},//不太好，格式经常有错
-        // { "name": "卧龙资源", "searchUrl": "https://collect.wolongzyw.com/api.php/provide/vod/" }, 非常恶心的广告
         { "name": "非凡资源", "searchUrl": "http://cj.ffzyapi.com/api.php/provide/vod/" },
         { "name": "量子资源", "searchUrl": "https://cj.lziapi.com/api.php/provide/vod/" },
         { "name": "ikun资源", "searchUrl": "https://ikunzyapi.com/api.php/provide/vod/from/ikm3u8/at/json/" },
         { "name": "光速资源", "searchUrl": "https://api.guangsuapi.com/api.php/provide/vod/from/gsm3u8/" },
         { "name": "高清资源", "searchUrl": "https://api.1080zyku.com/inc/apijson.php/" },
         { "name": "188资源", "searchUrl": "https://www.188zy.org/api.php/provide/vod/" },
-        // { "name": "飞速资源", "searchUrl": "https://www.feisuzyapi.com/api.php/provide/vod/" },//经常作妖或者没有资源
         { "name": "红牛资源", "searchUrl": "https://www.hongniuzy2.com/api.php/provide/vod/from/hnm3u8/" },
+        // { "name": "飞速资源", "searchUrl": "https://www.feisuzyapi.com/api.php/provide/vod/" },//经常作妖或者没有资源
+        // {"name":"闪电资源","searchUrl":"https://sdzyapi.com/api.php/provide/vod/"},//不太好，格式经常有错
+        // { "name": "卧龙资源", "searchUrl": "https://collect.wolongzyw.com/api.php/provide/vod/" }, 非常恶心的广告
         // {"name":"天空资源","searchUrl":"https://m3u8.tiankongapi.com/api.php/provide/vod/from/tkm3u8/"},//有防火墙，垃圾
         // { "name": "8090资源", "searchUrl": "https://api.yparse.com/api/json/m3u8/" },垃圾 可能有墙
         // { "name": "百度云资源", "searchUrl": "https://api.apibdzy.com/api.php/provide/vod/" },
@@ -133,7 +137,7 @@
     //播放按钮
     class PlayBtn {
         constructor() {
-            const e = htmlToElement(`<xy-button type="primary">一键播放</xy-button>`);
+            const e = htmlToElement(`<button class="liu-btn play-btn">一键播放</button>`);
             $(isMobile ? ".sub-original-title" : "h1").appendChild(e);
             const render = async (item) => {
                 const playList = await search(item.searchUrl);
@@ -142,14 +146,22 @@
                     e.loading = false;
                     new UI(playList);
                 }
+
                 //渲染资源列表
                 const btn = new SourceButton({ name: item.name, playList }).element;
+                if (!sourceSelected) {
+                    btn.classList.add("selected")
+                    sourceSelected = true
+                }
+
+
+
                 $(".sourceButtonList").appendChild(btn);
 
             };
             e.onclick = function () {
                 e.loading = true;
-                tip("正在搜索");
+                //tip("正在搜索");
                 searchSource.forEach(render);
                 setTimeout(() => {
                     if (e.loading == true) {
@@ -163,59 +175,26 @@
         }
     }
 
-    //影视源选择按钮
-    class SourceButton {
-        constructor(item) {
-            this.element = htmlToElement(`<xy-button class="source-selector" type="dashed">${item.name}</xy-button>`);
-            this.element.onclick = () => {
-                switchUrl(item.playList[seriesNum].url);
-                $(".series-select-space").remove();
-                new SeriesContainer(item.playList);
-            };
-            this.element._playList = item.playList
-            this.element._sourceName = item.name
-        }
-        //sources 是[{name:"..资源",playList:[{name:"第一集",url:""}]}]
-    }
 
-    //剧集选择器
-    class SeriesButton {
-        constructor(pNode, name, url, index) {
-            pNode.appendChild(htmlToElement(
-                `<xy-button class="series-selector" style="color:#a3a3a3" type="flat">${name}</xy-button>`
-            )).onclick = () => {
-                seriesNum = index;
-                switchUrl(url);
-                $(".show-series").innerText = `正在播放第${index + 1}集`;
-                speedTest()
-            };
-        }
-    }
-
-    //剧集选择器的container
-    class SeriesContainer {
-        constructor(playList) {
-            const e = htmlToElement(`<div class="series-select-space" style="display:flex;flex-wrap:wrap;overflow:scroll;align-content: start;"></div>`);
-            for (let [index, item] of playList.entries()) {
-                new SeriesButton(e, item.name, item.url, index);
-            }
-            $(".playSpace").appendChild(e);
-        }
-    }
 
     class UI {
         constructor(playList) {
             document.body.appendChild(htmlToElement(
                 `<div class="liu-playContainer">
-				<a class="liu-closePlayer">关闭界面</a>
-				<div class="sourceButtonList"></div>
-				<div class="playSpace" style="margin-top:1em;width:100%">
+				<button class="liu-closePlayer liu-btn">X</button>
+
+				<div class="playSpace" >
 					<div class="artplayer-app"></div>
+          <div class="series">
+            <div class="seletor-title">选集</div>
+            <div class="series-contianer"></div>
+          </div>
 				</div>
-				<div class="show-series" style="color:#a3a3a3"></div>
-				<p style="color:#a3a3a3">默认会播放第一个搜索到的资源，如果无法播放请尝试切换其他资源。</p>
-				<p style="color:#a3a3a3">部分影片选集后会出现卡顿，点击播放按钮或拖动一下进度条即可恢复。</p>
-				<a href="http://babelgo.cn:5230/m/1" target="_blank" style="color:#4aa150">❤️支持开发者</a>
+        <div class="sourceButtonList"></div>
+				<div class="mannul">
+          <div class="show-series" style="color:#a3a3a3"></div>
+          <a class="love-support liu-btn" href="http://babelgo.cn:5230/m/1" target="_blank" style="color:#4aa150">☕请我喝一杯咖啡吧，要饿死啦~</a>
+        </div>
 			</div>`
             )).querySelector(".liu-closePlayer").onclick = function () {
                 this.parentNode.remove();
@@ -233,8 +212,8 @@
     function initArt(url) {
         art = new Artplayer({
             container: ".artplayer-app",
-            url, pip: true,
-            autoSize: true,
+            url: url,
+            pip: true,
             fullscreen: true,
             fullscreenWeb: true,
             screenshot: true,
@@ -265,6 +244,55 @@
         });
         log(art)
     }
+
+
+    //影视源选择按钮
+    class SourceButton {
+        constructor(item) {
+            this.element = htmlToElement(`<button class="source-selector liu-btn" >${item.name}</button>`);
+            this.element.onclick = () => {
+                $(".selected") ? $(".selected").classList.remove("selected") : null;
+                this.element.classList.add("selected")
+                switchUrl(item.playList[seriesNum].url);
+                new SeriesContainer(item.playList);
+
+            };
+            this.element._playList = item.playList
+            this.element._sourceName = item.name
+        }
+        //sources 是[{name:"..资源",playList:[{name:"第一集",url:""}]}]
+    }
+
+    //剧集选择器
+    class SeriesButton {
+        constructor(pNode, name, url, index) {
+            let selector = htmlToElement(
+                `<button class="series-selector liu-btn" style="color:#a3a3a3" >${name.slice(0, 4)}</button>`
+            )
+            pNode.appendChild(selector).onclick = () => {
+                seriesNum = index;
+                switchUrl(url);
+                $(".playing") ? $(".playing").classList.remove("playing") : null;
+                // $(".show-series").innerText = `正在播放第${index + 1}集`;
+                selector.classList.add("playing")
+                speedTest()
+            };
+        }
+    }
+
+    //剧集选择器的container
+    class SeriesContainer {
+        constructor(playList) {
+            //const e = htmlToElement(`<div class="series-select-space" style="display:flex;flex-wrap:wrap;overflow:scroll;align-content: start;"></div>`);
+            const e = $(".series-contianer")
+            e.innerHTML = ""
+            for (let [index, item] of playList.entries()) {
+                new SeriesButton(e, item.name, item.url, index);
+            }
+            seriesNum == 0 ? $(".series-selector").classList.add("playing") : null;
+        }
+    }
+
 
 
     function switchUrl(url) {//兼容safari
@@ -352,7 +380,7 @@
 
     //对资源进行测速
     function speedTest() {
-        tip("开始测速")
+        // tip("脚本自动测试源的速度，随后请自行切换源进行尝试")
         let sourceButtons = $$(".source-selector")
         //log(sourceButtons)
         sourceButtons.forEach(async (e) => {
@@ -407,61 +435,144 @@
         });
     }
 
+    //按钮样式
     GM_addStyle(
-        `button {
-  border-radius: 8px;
+        `
+.liu-btn{
+  cursor:pointer;
+  font-size:1rem;
+  padding: 0.6rem 1.2rem;
   border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  background-color: #1a1a1a;
+}
+
+.play-btn {
+  border-radius: 8px;
   cursor: pointer;
-  transition: border-color 0.25s;
-   background-color:#f9f9f9;
+  font-weight: bolder;
+  background-color:#e8f5e9;
 }
-button:hover {
-  background-color:#f2f1f2;
+.play-btn:hover {
+  background-color:#c8e6c9;
 }
-  button:active{
-  border-color: #2f2f2f;
-  }`
+.play-btn:active{
+  background-color: #81c784;
+}
+
+.source-selector{
+  background-color: #141414;
+  color: #99a2aa;
+  padding:0.2rem 0.5rem;
+  margin:0.5rem 0.875rem;
+  border-radius:4px;
+}
+
+.series-selector{
+  background-color: #141414;
+  border-radius:3px;
+  color: #99a2aa;
+  width:3.5rem;
+  height:3.5rem;
+  font-size:0.75rem;
+  line-height:3.5rem;
+  padding:0;
+}
+
+.playing{
+  border:1px solid #4caf50;
+}
+
+.selected{
+  border:1px solid #4caf50;
+}
+
+
+.liu-closePlayer{
+  border-radius:3px;
+  background-color: #141414;
+	float:right;
+  color: #99a2aa;
+  width:2rem;
+  height:2rem;
+  line-height:2rem;
+  padding:0;
+  margin:0.5rem 1rem;
+}
+.liu-closePlayer:hover{
+  background-color:#1f1f1f;
+  color:white;
+}
+
+.love-support{
+  background-color:#141414;
+}
+.love-support:hover{
+  background-color:#1f1f1f;
+}
+
+
+`
+
     );
 
 
+    //剧集选择器布局
     GM_addStyle(
-        `.TalionNav{
+        `
+.series-contianer{
+  display:grid;
+  grid-template-columns: repeat(5,1fr);
+  grid-column-gap:0.5rem;
+  grid-row-gap:0.5rem;
+  margin-top:1rem;
+}
+@media screen and (max-width: 1025px) {
+.series-contianer{
+  display:grid;
+  grid-template-columns: repeat(7,1fr);
+  grid-column-gap:0.5rem;
+  grid-row-gap:0.5rem;
+  margin-top:1rem;
+}
+
+}
+
+
+`
+    )
+
+
+    //布局
+    GM_addStyle(
+        `
+.TalionNav{
 	z-index:10;
 }
 .speed-slow{
-	color:#c62828;
+	color:#9e9e9e;
 }
 .speed-fast{
 	color:#4aa150;
 }
 
-.source-selector{
-	margin:0.5em;
+
+
+.mannul{
+  margin:1rem;
+  font-size:1rem;
 }
-.series-selector{
-	margin:0.5em;
-}
+
 
 
 .liu-playContainer{
 	width:100%;
 	height:100%;
-	background-color:#121212;
+	background-color:#1c2022;
 	position:fixed;
 	top:0;
 	z-index:11;
 }
 
-.liu-closePlayer{
-	float:right;
-	margin-inline:10px;
-	color:white;
-}
+
 
 .video-selector{
 	display:flex;
@@ -489,63 +600,29 @@ button:hover {
 .liu-light{
 	background-color:#7bed9f;
 }
-.liu-btn {
-	width: 6.5em;
-	height: 2em;
-	margin: 0.5em;
-	background: #41ac52;
-	color: white;
-	border: none;
-	border-radius: 0.625em;
-	font-size: 20px;
-	font-weight: bold;
-	cursor: pointer;
-	position: relative;
-	z-index: 1;
-	overflow: hidden;
-}
 
-.liu-btn:hover {
-	color: #41ac52;
-}
 
-.liu-btn:after {
-	content: '';
-	background: white;
-	position: absolute;
-	z-index: -1;
-	left: -20%;
-	right: -20%;
-	top: 0;
-	bottom: 0;
-	transform: skewX(-45deg) scale(0, 1);
-	transition: all 0.5s;
-}
 
-.liu-btn:hover:after {
-	transform: skewX(-45deg) scale(1, 1);
-	-webkit-transition: all 0.5s;
-	transition: all 0.5s;
-}
-xy-button{
-	margin:0em 1em 0em 0em;
-	height:1.5em;
-	cursor:pointer;
-}
+
 .playSpace{
 	display: grid;
 	height:500px;
-	grid-template-rows: 1fr;
-	grid-template-columns: 70% 30%;
+  margin:1rem;
+	grid-template-columns: 2fr 1fr;
 	grid-row-gap:0px;
-	grid-column-gap:0px;
+	grid-column-gap:1rem;
+  margin-top:2rem;
+  clear: both;
 }
 .series-select-space::-webkit-scrollbar {display:none}
 .series-select-space{
 	height:500px;
 }
+
 .artplayer-app{
+
 	height:500px;
+
 }
 @media screen and (max-width: 1025px) {
 	.playSpace{
@@ -553,7 +630,7 @@ xy-button{
 		height:700px;
 		grid-template-rows: 1fr 1fr;
 		grid-template-columns:1fr;
-		grid-row-gap:0px;
+		grid-row-gap:10px;
 		grid-column-gap:0px;
 	}
 	.series-select-space{
@@ -562,7 +639,19 @@ xy-button{
 	.artplayer-app{
 		height:400px;
 	}
-}`
+}
+
+.seletor-title{
+  height:3rem;
+  line-height:3rem;
+  background-color: #141414;
+  color:#fafafa;
+  font-size:1.25rem;
+  padding: 0 1rem;
+}
+
+
+`
     );
     new PlayBtn();
     addScript();
